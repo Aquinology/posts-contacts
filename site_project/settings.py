f0b377b8942767c801c7ae57258d7faf0b4bfd6a
Aquinology <riabova_e@iuca.kg>
2020-09-30 00:33:40 +0600

    Templates in the project directory

--- site_project/settings.py
+++ site_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'site_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'site_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
